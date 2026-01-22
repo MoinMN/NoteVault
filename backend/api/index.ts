95316e163df6002c7d0cbc3f4f13221c15bd3d29
MoinMN <moinnaik45@eng.rizvi.edu.in>
2026-01-22 17:21:57 +0530

    updated website

---
+++ backend/api/index.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+
+export default async function handler(req: Request, res: Response) {
+  const filePath = path.join(process.cwd(), "public", "index.html");
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send(html);
+}
